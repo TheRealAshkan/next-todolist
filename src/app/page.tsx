392d1d9d86346fc96e5eb94cb32b7b93d4af302b
AshkanMahdizadeh <ac.programmer.ashkan@gmail.com>
2025-04-09 23:17:49 +0330

    add components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,28 @@
-import Image from "next/image";
+import TodoInput from "@/components/TodoInput";
+import {
+  Card,
+  CardContent,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card"
+
+import { Label } from "@/components/ui/label"
 
 export default function Home() {
   return (
-    <>
-    
-      <h1>فعالیت‌های من</h1>
-    </>
+    <div className="flex items-center justify-center min-h-screen bg-gray-100">
+      <Card className="w-1/3">
+        <CardHeader>
+          <CardTitle>فعالیت‌های من</CardTitle>
+        </CardHeader>
+        <CardContent>
+          <TodoInput />
+        </CardContent>
+        <CardFooter>
+          <p>Card Footer</p>
+        </CardFooter>
+      </Card>
+    </div>
   );
 }
